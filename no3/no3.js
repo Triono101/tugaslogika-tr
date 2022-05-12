626e44ecf4a5a87728763aa08c4d90bbec4467d8
Triono <triono2125@gmail.com>
2022-05-12 23:49:50 +0700

    updating revision task

--- no3/no3.js
+++ no3/no3.js
@@ -1,10 +1,13 @@
-function printDigitValue(value) {
-    var array = value.split('');
-    for(var i = 0; i<array.length; i++) {
-     if(!isNaN(array[i])) {
-       console.log(array[i]);
-     } 
-    }
-  }
-  
-printDigitValue("9.86-A5.321");
+var n = "9.86-A5.321"
+var d = n.replace(/\D/g,"").split("").reverse();
+
+let string = "";
+
+function valueOutput(data, n, str) {
+    return data+Array(n-String(data).length+1).join(str||'0');
+}
+for(let i=d.length-1; i>=0; i--) {
+  string += parseInt(valueOutput(d[i] , i+1))+ "\n";
+}
+
+console.log(string);
--- no4/no4.js
+++ no4/no4.js
@@ -1,21 +1,5 @@
-//Pilihan Pertama NILAI SUDAH FIX
-
 let a = 3, b = 7;
 [a, b] = [b, a];
 
-console.log(`Nilai variabel swap dari 3 dan 7 adalah ${a} dan ${b}`);
-alert(`Nilai variabel swap dari 3 dan 7 adalah ${a} dan ${b}`);
-
-//Pilihan Kedua TENTUKAN NILAI SENDIRI
-
-let c = prompt('Masukkan nilai variabel c : ');
-let d = prompt('Masukkan nilai variabel d : ');
-
-let temp;
-temp = c;
-c = d;
-d = temp;
-
-console.log(`Nilai variabel swap c : ${c}`);
-console.log(`Nilai variabel swap d : ${d}`);
-alert(`Nilai variabel swap dari ${c} dan ${d} adalah ${d} dan ${c}`);
+console.log(`let a = 3, let b = 7 => (a = ${a}, b = ${b})`);
+document.write(`let a = 3, let b = 7 => (a = ${a}, b = ${b})`);
