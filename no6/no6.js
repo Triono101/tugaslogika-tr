e6b94cc8d1cb86191536ddaaecb1abd36f7377e1
Triono <triono2125@gmail.com>
2022-05-19 23:15:40 +0700

    update necessary code

--- no6/no6.js
+++ no6/no6.js
@@ -3,5 +3,5 @@ angka.sort(function(a, b) {
     return a - b;
 });
 
-alert(`Hasil sortir [105, 3, 45, 1, 5, 20, 10, 75, 50, 100] adalah : ` + angka);
+document.write(`Hasil sortir dari [105, 3, 45, 1, 5, 20, 10, 75, 50, 100] adalah : ` + angka);
 console.log(angka);
