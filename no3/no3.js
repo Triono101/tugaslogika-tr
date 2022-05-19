3e4ff0a819ff5e593c720eac60a227c394d673bc
Triono <triono2125@gmail.com>
2022-05-19 23:17:23 +0700

    updating revision task

--- no3/no3.js
+++ no3/no3.js
@@ -1,14 +1,22 @@
+// var data = "9.86-A5.321"
+// var option = data.replace(/\D/g,"").split("").reverse();
+
+// let string = "";
+
+// function valueOutput(n, data, str) {
+//     return n + Array(data-String(data).length + 1).join(str||'0');
+// }
+// for(let i = option.length-1; i >= 0; i--) {
+//   string += parseInt(valueOutput(option[i] , i+1))+ "\n";
+// }
+// console.log(string);
+
 var data = "9.86-A5.321"
-var option = data.replace(/\D/g,"").split("").reverse();
+var option = data.replace(/\D/g,"");
+console.log(option);
 
-let string = "";
-
-function valueOutput(n, data, str) {
-    return n + Array(data-String(data).length + 1).join(str||'0');
-}
-
-for(let i = option.length-1; i >= 0; i--) {
-  string += parseInt(valueOutput(option[i] , i+1))+ "\n";
-}
-
-console.log(string);
+var valueOutput = option.split("");
+const number = valueOutput.length;
+for(let i=0; i<number; i++) {
+  console.log(valueOutput[i].padEnd(number-i,'0'));
+};
